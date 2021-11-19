63f4c0b1ba91310b5a0a6165f8c3b21803343745
onno204 <15333546+onno204@users.noreply.github.com>
2021-11-19 05:18:31 +0100

    debug

--- startFrontend.js
+++ startFrontend.js
@@ -4,6 +4,7 @@ const child_process = require('child_process');
 const config = dotenv.config()
 const FRONTEND_PORT = process.env.PORT || process.env.FRONTEND_PORT || 4200;
 
-const child = child_process.exec(`cd Frontend && ng serve --port=${FRONTEND_PORT}`);
+console.log("Starting ng with command:", `cd Frontend && npm run start -- --port=${FRONTEND_PORT}`)
+const child = child_process.exec(`cd Frontend && npm run start -- --port=${FRONTEND_PORT}`);
 child.stderr.on('data', err => console.error(err));
 child.stdout.on('data', data => console.log(data.toString()));
