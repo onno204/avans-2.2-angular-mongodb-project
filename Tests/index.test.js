2aa7c07b78aa28f6faad97013d3978e38ccada61
onno204 <15333546+onno204@users.noreply.github.com>
2022-04-09 18:35:04 +0200

    Added tests

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -9,8 +9,14 @@ const randomstring = require("randomstring");
 const apiUrl = `http://localhost:8080/api/v1`;
 // Not the "@admin-test.nl" gets elevated to admin
 const testData = {
-    testUsername: `test-${randomstring.generate(7)}@admin-test.nl`,
-    testPassword: randomstring.generate()
+    testUsername: `test-${randomstring.generate(10)}@admin-test.nl`,
+    testUsername2: `test2-${randomstring.generate(10)}@admin-test.nl`,
+    testUsername3: `test3-${randomstring.generate(10)}@admin-test.nl`,
+    testPassword: randomstring.generate(),
+    testDevice: `device-${randomstring.generate(10)}`,
+    testDevice2: `device2-${randomstring.generate(10)}`,
+    testApp: `app-${randomstring.generate(10)}`,
+    testApp2: `app2-${randomstring.generate(10)}`,
 };
 console.log("Using test data: ", testData)
 
@@ -54,13 +60,51 @@ describe("Avans Apps Test", () => {
             })
         });
 
+        it("should register a second user", (done) => {
+            axios.post(`${apiUrl}/register`, {
+                email: testData.testUsername2,
+                password: testData.testPassword,
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data.token).to.be.a('string')
+                    testData['userId2'] = response.data.data.user_id
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                done(error);
+            })
+        });
+
+        it("should register a third user", (done) => {
+            axios.post(`${apiUrl}/register`, {
+                email: testData.testUsername3,
+                password: testData.testPassword,
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data.token).to.be.a('string')
+                    testData['userId3'] = response.data.data.user_id
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                done(error);
+            })
+        });
+
+
         it("should login as an user", (done) => {
             axios.post(`${apiUrl}/login`, {
                 email: testData.testUsername,
                 password: testData.testPassword,
             }).then((response) => {
                 try {
-                    console.log("response", response.data);
                     assert.equal(response.status, 200);
                     expect(response.data.success).to.equal(true);
                     expect(response.data.data.token).to.be.a('string')
@@ -72,12 +116,276 @@ describe("Avans Apps Test", () => {
                 }
                 done()
             }).catch((error) => {
+                done(error);
+            })
+        });
+    });
+
+    describe("should verify the user management routes", () => {
+        it("should get all users of the platform, including its self", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.get(`${apiUrl}/users`).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(JSON.stringify(response.data.data)).to.contain(testData.testUsername);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to update a user", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.put(`${apiUrl}/users/${testData['userId2']}`, {
+                role: 'user'
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 202);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data.role).to.equal('user');
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to delete a user", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.delete(`${apiUrl}/users/${testData['userId2']}`).then((response) => {
+                try {
+                    assert.equal(response.status, 202);
+                    expect(response.data.success).to.equal(true);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
                 console.log("Api Error", error.response.data);
                 done(error);
             })
         });
     });
-    describe("should verify the Authorization routes", () => {
+
+    describe("should verify the device management routes", () => {
+        it("should be able to add a device", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.post(`${apiUrl}/devices`, {
+                'name': testData['testDevice']
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 201);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data._id).to.be.a('string')
+                    testData['deviceId'] = response.data.data._id
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to get all devices", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.get(`${apiUrl}/devices`).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(JSON.stringify(response.data.data)).to.contain(testData.testDevice);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to update a device", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.put(`${apiUrl}/devices/${testData['deviceId']}`, {
+                'name': testData['testDevice2']
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 202);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data._id).to.equal(testData['deviceId']);
+                    expect(response.data.data.name).to.equal(testData['testDevice2']);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to delete a device", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.delete(`${apiUrl}/devices/${testData['deviceId']}`).then((response) => {
+                try {
+                    assert.equal(response.status, 202);
+                    expect(response.data.success).to.equal(true);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+    });
+
+    describe("should verify the App management routes", () => {
+        it("should be able to add a app", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.post(`${apiUrl}/apps`, {
+                'name': testData['testApp'],
+                'description': `description-${testData['testApp']}`,
+                'deviceName': testData['testDevice2'],
+                'category': `category-${testData['testApp']}`,
+                'public': false,
+                'icon': "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 201);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data._id).to.be.a('string')
+                    testData['appId'] = response.data.data._id
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should not be able to add an app with the same name", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.post(`${apiUrl}/apps`, {
+                'name': testData['testApp'],
+                'description': `description-${testData['testApp']}`,
+                'deviceName': testData['testDevice2'],
+                'category': `category-${testData['testApp']}`,
+                'public': false,
+                'icon': "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+            }).then((response) => {
+                return done(new Error("shouldn't come in this block"))
+            }).catch((error) => {
+                try {
+                    assert.equal(error.response.status, 400);
+                    expect(error.response.data.success).to.equal(false);
+                } catch (err) {
+                    return done(err)
+                }
+                done();
+            });
+        });
+
+        it("should be able to add a second app", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.post(`${apiUrl}/apps`, {
+                'name': testData['testApp2'],
+                'description': `description-${testData['testApp2']}`,
+                'deviceName': testData['testDevice2'],
+                'category': `category-${testData['testApp2']}`,
+                'public': false,
+                'icon': "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 201);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data._id).to.be.a('string')
+                    testData['appId2'] = response.data.data._id
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to fetch a single app", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.get(`${apiUrl}/apps/${testData['appId']}`).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data.public).to.equal(false)
+                    expect(response.data.data.description).to.equal(`description-${testData['testApp']}`)
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to fetch all apps", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.get(`${apiUrl}/apps?nolimit=1`).then((response) => {
+                try {
+                    assert.equal(response.status, 200);
+                    expect(response.data.success).to.equal(true);
+                    expect(JSON.stringify(response.data.data)).to.contain(`description-${testData['testApp']}`);
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+        it("should be able to update a app", (done) => {
+            const axiosAuthed = axios.create({headers: {'Authorization': `Bearer ${testData['token']}`}});
+            axiosAuthed.put(`${apiUrl}/apps/${testData['appId']}`, {
+                'description': `description2-${testData['testApp']}`,
+                'public': true,
+            }).then((response) => {
+                try {
+                    assert.equal(response.status, 202);
+                    expect(response.data.success).to.equal(true);
+                    expect(response.data.data.public).to.equal(true)
+                    expect(response.data.data.description).to.equal(`description2-${testData['testApp']}`)
+                } catch (err) {
+                    return done(err)
+                }
+                done()
+            }).catch((error) => {
+                console.log("Api Error", error.response.data);
+                done(error);
+            })
+        });
+
+
+    });
+
+    describe("Finals", () => {
+        it("should print the final test data", () => {
+            console.log('Final test-data', testData)
+        });
     });
 });
 
